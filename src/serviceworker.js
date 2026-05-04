b41afa0aade4c281a63bfd4e49cb54686b13ba63
softwaremuregro <softwaremuregro@gmail.com>
2026-05-03 21:41:55 -0600

    linea para que se agregue el .indexhtml en el servi3ceworker

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -4,7 +4,7 @@ const CACHE_NAME = `cache-${version}`;
 
 // build = los archivos que genera SvelteKit (JS, CSS de las páginas)
 // files = todo lo que está en la carpeta 'static' (Bootstrap local, imágenes, etc.)
-const ASSETS = [...build, ...files];
+const ASSETS = [...build, ...files,'/'];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
